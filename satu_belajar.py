1e992194cf7e0ad61f00033cc1b1a372ca3ca755
mataramis <buattryout44@gmail.com>
2022-12-21 04:18:44 +0700

    budi

--- satu_belajar.py
+++ satu_belajar.py
@@ -1,1 +1,22 @@
-print("hello world")
+# sekuensial
+print('ibu berkata, "pergi ke toko"')
+print('budi menjawab, "baik bu apa yang harus saya beli di toko" ')
+print('ibu menjawab, "beli satu botol susu, dan jika ada telur beli 6"')
+print("budi berangkat ke toko ")
+print("budi mulai berbelanja\n\n")
+
+# percabangan
+jumlah_telur = 5
+jumlah_botol_susu = 5
+print("budi menegecek di warung apakah ada susu")
+if jumlah_botol_susu > 0 :
+    print("budi mengecek harga nya dan ternyata uang nya cukup")
+    if jumlah_telur > 0:
+        print("budi membeli 6 botol susu")
+    else:
+        print("budi membeli 1 botol susu")
+else:
+    print("budi tidak jadi membeli susu")
+
+print("budi pulang ke rumah ")
+print("budi menyerahkan belanja ke ibu")
