152c52d9c7f64f85323b051e19ebee787efd683b
mataramis <buattryout44@gmail.com>
2023-01-11 19:31:21 +0700

    program perulangan while

---
+++ 3. perulangan while - rwid.py
@@ -0,0 +1,9 @@
+jumlah_buku = 10
+
+print(f"ibu suruh baca buku")
+buku_terbaca = 0
+print(f"buku terbaca{buku_terbaca}")
+
+while buku_terbaca < jumlah_buku:
+    buku_terbaca = buku_terbaca + 1
+    print(f"buku ke - {buku_terbaca}")
