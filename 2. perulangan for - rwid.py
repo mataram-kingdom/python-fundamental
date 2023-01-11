2e2fd0701aba7f7236865d242f3bcac23d84b40a
mataramis <buattryout44@gmail.com>
2023-01-11 19:25:43 +0700

    program perulangan for

---
+++ 2. perulangan for - rwid.py
@@ -0,0 +1,11 @@
+#perulangan for
+
+jumlah_buku = 10
+print("ibu suruh baca buku")
+buku_terbaca = 0
+
+print(f"jumlah buku terbaca {buku_terbaca}")
+for buku_terbaca in range(1,jumlah_buku+1):
+    print(f"buku ke-{buku_terbaca} sudah dibaca")
+
+print(f"jumlah buku terbaca {buku_terbaca}")
