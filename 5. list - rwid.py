a7c84ed5b8e8ef8887506f23d0032d3b0694bab4
mataramis <buattryout44@gmail.com>
2023-01-22 01:19:16 +0700

    list awal

---
+++ 5. list - rwid.py
@@ -0,0 +1,14 @@
+
+# menambahkan elemen - append
+daftar_buku = ["heri potter, jujur kasian, gatotkaca, bima satria"]
+daftar_buku.append("legenda malin kundang")
+print(daftar_buku)
+
+print("========= dengan perulangan =========")
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+# menghilangkan elemen - clear
+daftar_buku.clear()
+print(daftar_buku)
+
