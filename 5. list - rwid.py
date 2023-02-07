521cffe12a47a8dd5b4525fbedecaae93eef242f
mataramis <buattryout44@gmail.com>
2023-02-07 22:39:18 +0700

    tambah tampilkan index tertentu dari list

--- 5. list - rwid.py
+++ 5. list - rwid.py
@@ -1,14 +1,20 @@
 
 # menambahkan elemen - append
-daftar_buku = ["heri potter, jujur kasian, gatotkaca, bima satria"]
+daftar_buku = ["heri potter", "jujur kasian", "gatotkaca", "bima satria"]
 daftar_buku.append("legenda malin kundang")
-print(daftar_buku)
+print(daftar_buku )
 
-print("========= dengan perulangan =========")
+print("-- dengan perulangan (pengolahan data ) =========")
 for i in range(0, len(daftar_buku)):
     print(daftar_buku[i])
 
+# menampilkan dengan index tertentu
+print(" -- menampilkan dengan index tertentu")
+print(daftar_buku[3])
+
+
 # menghilangkan elemen - clear
+print(' -- menghilangkan elemen')
 daftar_buku.clear()
 print(daftar_buku)
 
