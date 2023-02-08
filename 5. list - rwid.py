d3180aa0d743e42ce323e5524655fc3045db041e
mataramis <buattryout44@gmail.com>
2023-02-09 00:46:53 +0700

    tambah sampai list comperhension (membuat list baru )

--- 5. list - rwid.py
+++ 5. list - rwid.py
@@ -46,8 +46,8 @@ for i in range(0,len(daftar_buku)):
 
 
 print('\n- perintah del')
-print('\t\t\tdaftar asli')
-daftar_buku = [ "negri bedebah", "negri ujung tanduk", "bumi", "bulan, matahari"]
+print('\t\t\t\t\t\tdaftar asli')
+daftar_buku = [ "negri bedebah", "negri ujung tanduk", "bumi", "bulan"]
 for i in range(0,len(daftar_buku)):
     print(daftar_buku[i])
 
@@ -68,6 +68,36 @@ del daftar_buku[0::2]
 for i in range(0,len(daftar_buku)):
     print(daftar_buku[i])
 
+print('\n - membuat list baru')
+daftar_buku = [ "negri bedebah", "negri ujung tanduk", "bumi", "bulan"]
+daftar_buku_baru = daftar_buku[:]
+for i in range(0,len(daftar_buku)):
+    print(daftar_buku[i])
+
+
+print('\n - hasil membuat list baru')
+del daftar_buku[:]
+for i in range(0,len(daftar_buku_baru)):
+    print(daftar_buku_baru[i])
+
+print("")
+print('\n - membuat list baru dengan comperhension: ganjil')
+daftar_buku = [ "1 negri bedebah", "2 negri ujung tanduk", "3 bumi", "4 bulan"]
+daftar_buku_baru = daftar_buku[0::2]
+for i in range(0,len(daftar_buku_baru)):
+    print(daftar_buku_baru[i])
+
+print('\n - membuat list baru dengan comperhension: genap')
+daftar_buku = [ "1 negri bedebah", "2 negri ujung tanduk", "3 bumi", "4 bulan"]
+daftar_buku_baru = daftar_buku[1::2]
+for i in range(0,len(daftar_buku_baru)):
+    print(daftar_buku_baru[i])
+
+print('\n - membuat list baru dengan buang di ujung')
+daftar_buku = [ "1 negri bedebah", "2 negri ujung tanduk", "3 bumi", "4 bulan"]
+daftar_buku_baru = daftar_buku[0:-1:2]
+for i in range(0,len(daftar_buku_baru)):
+    print(daftar_buku_baru[i])
 
 
 
