9620d376bfabb1a6cbcc93b3de3845c4bb48c43b
mataramis <buattryout44@gmail.com>
2023-02-09 00:56:31 +0700

    buat dictionary

---
+++ 6. dictionary - rwid.py
@@ -0,0 +1,17 @@
+users = {
+    "id" : "1",
+    "name" : "muhammad afrizal",
+    "username" : "zale",
+    "email" : "hohohoho",
+    "address" : {
+        "desa" : "rejosa",
+        "kec" : "pakis",
+        "kab" : "magel",
+    }
+}
+print(users)
+print(users['name'])
+print(users['username'])
+print(users['email'])
+print(users['id'])
+print(users['address']['kab'])
