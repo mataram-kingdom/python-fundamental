f6c2b7b64a446beb11dc33fd9c703fd355403cc1
mataramis <buattryout44@gmail.com>
2023-02-09 01:00:05 +0700

    buat dictionary import json

--- 6. dictionary - rwid.py
+++ 6. dictionary - rwid.py
@@ -15,3 +15,15 @@ print(users['username'])
 print(users['email'])
 print(users['id'])
 print(users['address']['kab'])
+
+print(users)
+print(type(users))
+print("\n ubah dictionary ke json")
+
+import json
+result = json.dumps(users)
+print(type(result))
+print(result)
+
+with open('result.json', 'w') as file :
+    json.dump(users,file)
