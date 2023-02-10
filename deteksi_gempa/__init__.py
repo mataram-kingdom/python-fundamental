f5d3eff4e7c3ce8a86973f7f07cd58a2be98b8f0
mataramis <buattryout44@gmail.com>
2023-02-11 00:45:48 +0700

    done of beautifulsoup adn requast with apk

--- deteksi_gempa/__init__.py
+++ deteksi_gempa/__init__.py
@@ -12,20 +12,59 @@ def ekstraksi_data():
     except  Exception:
         return None
     if r.status_code == 200 :
-        print(r.text)
-        print(r.status_code)
+        # print(r.text)
+        # print(r.status_code)
 
-    # soup = BeautifulSoup("<p>Some<b>bad<i>HTML")
-    # print(soup.prettify()  )
+        soup = BeautifulSoup(r.text,'html.parser')
+
+        result = soup.findChild('ul', {'class':'list-unstyled'})
+        result = result.findChildren('li')
+        print(f'============ list pencaian : ==============\n')
+        j = 0
+        mag = None
+        dalam = None
+        koordinat = None
+        lokasi = None
+        ket = None
+
+        for i in result:
+            print(j,i)
+            if j == 1:
+                mag = i.text
+            elif j == 2:
+                dalam = i.text
+            elif j ==3:
+                koordinat = i.text.split(' - ')
+                ls = koordinat[0]
+                bt = koordinat[1]
+            elif j == 4:
+                lokasi = i.text
+            elif j ==5:
+                ket = i.text
+
+
+            j = j +1
+
+        title = soup.find('title')
+        tanggal = soup.find('span',{'class': 'waktu'})
+        waktu = tanggal.text.split(', ')[1]
+        # mag = soup.find('ul',{'class': 'list-unstyled'})
+        print(title.string)
+        print("==================================")
+
+
+
         hasil = dict()
-        hasil['tanggal'] = '09 februari 2023'
-        hasil['waktu'] = '23:57:12 WIB'
-        hasil['magnitudo'] = '3.0'
-        hasil['radius'] = '8 km'
-        hasil['lokasi'] = '2.54 LS - 140.68 BT'
-        hasil['pusat'] = 'Pusat gempa berada di darat 4 km BaratDaya Kota Jayapura'
-        hasil['dirasakan'] = 'Dirasakan (Skala MMI): II Kota Jayapura'
+        hasil['tanggal'] = tanggal.text
+        hasil['waktu'] = waktu
+        hasil['mag'] = mag
+        hasil['kedalaman'] = dalam
+        hasil['koordinat'] = {'ls': ls, 'bt': bt}
+        hasil['lokasi'] = lokasi
+        hasil['dirasakan'] = ket
+
         return hasil
+        print("=================================")
     else:
         return None
 
@@ -36,13 +75,13 @@ def tampilkan_data(result):
         return
 
     print("gempa berdasarkan bmkg")
-    print(f'tanggal {result["tanggal"]}')
-    print(f'waktu {result["waktu"]}')
-    print(f'magnitudo {result["magnitudo"]}')
-    print(f'radius {result["radius"]}')
-    print(f'loc {result["lokasi"]}')
-    print(f'pusat {result["pusat"]}')
-    print(f'ket {result["dirasakan"]}')
+    print(f'tanggal \t : {result["tanggal"]}')
+    print(f'waktu \t\t : {result["waktu"]}')
+    print(f'magnitudo \t : {result["mag"]}')
+    print(f'kedalaman \t : {result["kedalaman"]}')
+    print(f'koordinat \t : LS : {result["koordinat"]["ls"]}, BT: {result["koordinat"]["bt"]}')
+    print(f'lokasi \t\t : {result["lokasi"]}')
+    print(f'ket \t\t : {result["dirasakan"]}')
 
 # if __name__ = '__main__':
 #     print("hai")
